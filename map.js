46dbcd8e306dde0667407c4a66f27dce66d231e4
Daniël Louwrink <daniel.louwrink@gmail.com>
2018-12-10 01:53:00 +0100

    get spawn position

--- map.js
+++ map.js
@@ -14,6 +14,18 @@ function getExportMapping(module) {
     return getAddress;
 }
 
+function deadbeef(size) {
+    var rounded = Math.ceil(size / 4);
+    var mem = Memory.alloc(rounded);
+
+    var beef = new NativePointer("0xdeadbeef");
+    for (var offset = 0; offset < rounded / 4; offset += 4) {
+        Memory.writePointer(mem.add(offset), beef);
+    }
+
+    return mem;
+}
+
 var module = 'libminecraftpe.so';
 var baseAddress = Module.findBaseAddress(module);
 var getAddress = getExportMapping(module);
@@ -26,25 +38,30 @@ var biomeSourceConstructor = new NativeFunction(getAddress('_ZN11BiomeSourceC2Ej
                                                 ['pointer',
                                                  'int32',
                                                  'pointer',
-                                                 ['pointer', 'pointer'],
-                                                 ['pointer', 'pointer']]);
+                                                 'pointer',
+                                                 'pointer']);
 
 var createDefaultLayers = new NativeFunction(getAddress('_ZN18OverworldDimension19createDefaultLayersExRSt10shared_ptrI5LayerES3_13GeneratorTypeb'),
                                              'void',
                                              ['int64', 'pointer', 'pointer', 'int32', 'uint32']);
 
-// var getSpawn =
-//     new NativeFunction(getAddress('_ZNK11BiomeSource16getSpawnPositionEv'),
-//                        'void', ['pointer', 'pointer']);
+var findValidSpawnPosition = new NativeFunction(getAddress('_Z22findValidSpawnPositionR11BiomeSourceiiii'),
+                                                'int32',
+                                                ['pointer',
+                                                 'pointer',
+                                                 'int32',
+                                                 'int32',
+                                                 'int32',
+                                                 'int32']);
 
 var fillArea =
     new NativeFunction(getAddress('_ZN15OceanMixerLayer8fillAreaER9LayerDataiiii'),
                        'void', ['pointer', 'pointer', 'int', 'int', 'int', 'int']);
 
 function allocLayerData(size) {
-    let layerData = Memory.alloc(8 + size);
-    let left = layerData.add(8);
-    let right = layerData.add(8 + size / 2);
+    var layerData = Memory.alloc(8 + size);
+    var left = layerData.add(8);
+    var right = layerData.add(8 + size / 2);
     Memory.writePointer(layerData, left);
     Memory.writePointer(layerData.add(4), right);
     return layerData;
@@ -55,7 +72,7 @@ function createSourceAndLayers(seed) {
     var layer2 = Memory.alloc(8);
     createDefaultLayers(seed, layer1, layer2, 1, 0);
     var source = Memory.alloc(8192);
-    biomeSourceConstructor(source, seed, biomePlains, [Memory.readPointer(layer1), Memory.readPointer(layer1.add(4))], [Memory.readPointer(layer2), Memory.readPointer(layer2.add(4))]);
+    biomeSourceConstructor(source, seed, biomePlains, Memory.dup(layer1, 8), Memory.dup(layer2, 8));
 
     return {
         biomeSource: source,
@@ -91,12 +108,28 @@ function createSourceAndLayers(seed) {
 // function layerFromBiomeSource(source) {
 //     return Memory.readPointer(source.add(4));
 // }
+function vtable(ptr) {
+    return Memory.readPointer(ptr).sub(baseAddress);
+}
 
-// function spawnPosition(source) {
-//     var spawnX = Memory.readS32(source.add(52));
-//     var spawnZ = Memory.readS32(source.add(60));
-//     return [spawnX, spawnZ];
-// }
+function findSpawn(source) {
+    var spawnX = 0;
+    var spawnZ = 0;
+
+    var pos = Memory.alloc(1024);
+
+    while (true) {
+        var v = findValidSpawnPosition(pos, source, spawnX, spawnZ, 10, 4);
+
+        if (v != 0) {
+            break;
+        }
+
+        spawnX += 40;
+    }
+
+    return [Memory.readS32(pos.add(4)), Memory.readS32(pos.add(12))];
+}
 
 function getBiomes(layer, layerData, x, z) {
     fillArea(layer, layerData, x, z, BIOME_TILE_SIZE, BIOME_TILE_SIZE);
@@ -108,10 +141,10 @@ function getBiomes(layer, layerData, x, z) {
 function sendSeedData(layerData, seed) {
     var stuff = createSourceAndLayers(seed);
     var source = stuff.biomeSource;
-    var spawn = [0, 0];
+    var spawn = findSpawn(source);
     var layer = stuff.layer1;
 
-    var radius = 50;
+    var radius = 10;
 
     var x = Math.round(spawn[0] / 4) - radius * BIOME_TILE_SIZE;
     var z = Math.round(spawn[1] / 4) - radius * BIOME_TILE_SIZE;
@@ -160,17 +193,39 @@ function intercept() {
     var addr = getAddress('_ZN11BiomeSourceC2EjR5BiomeSt10shared_ptrI5LayerES4_');
     Interceptor.attach(addr, {
         onEnter: function (args) {
-            console.log(Memory.readPointer());
-            console.log('foo: ' + args[2]);
+            console.log(hexdump(args[3]));
+            console.log(hexdump(args[4]));
+        }
+    });
+
+    var addr = getAddress('_ZN18OverworldDimension19createDefaultLayersExRSt10shared_ptrI5LayerES3_13GeneratorTypeb');
+    Interceptor.attach(addr, {
+        onEnter: function (args) {
+            console.log('createdefaultlayers');
+            console.log('layer1 location: ' + args[2]);
+            this.layer1 = args[2];
+            console.log('layer2 location: ' + args[3]);
+            this.layer2 = args[3];
+        },
+        onLeave: function (retval) {
+            console.log('layer1 data: ' + Memory.readPointer(this.layer1));
+            console.log('layer1 addit: ' + Memory.readPointer(this.layer1.add(4)));
+            console.log('layer1 vtable: ' + vtable(Memory.readPointer(this.layer1)));
+
+            console.log('layer2 data: ' + Memory.readPointer(this.layer2));
+            console.log('layer2 addit: ' + Memory.readPointer(this.layer2.add(4)));
+            console.log('layer2 vtable: ' + vtable(Memory.readPointer(this.layer2)));
         }
     });
 }
 
 function main() {
+    console.log('foo');
     var layerData = allocLayerData(1000000);
-    var seed = 37286;
+    var seed = 1337;
     sendSeedData(layerData, seed);
 }
 
-setTimeout(main, 500);
 console.log('hook');
+console.log(baseAddress);
+setTimeout(main, 500);
